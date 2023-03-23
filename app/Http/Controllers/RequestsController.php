9893d4cca87e28b4c1f8deae92f4cf8052c6ceca
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-03-24 02:48:59 +0700

    penambahan endpoint

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -2,28 +2,72 @@
 
 namespace App\Http\Controllers;
 use App\Models\Requests;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\QueryException;
+use App\Http\Resources\RequestsResource;
 
 class RequestsController extends Controller
 {
     public function index(){
-        $respons = DB::table('requests')
-        ->join('rs', 'requests.rs_id', '=', 'rs.id')
-        ->get();
-        return response()->json(['data' => $respons]);
+        try {
+            $respons = DB::table('requests')
+            ->join('rs', 'requests.rs_id', '=', 'rs.id')
+            ->join('users', 'requests.user_id', '=', 'users.id')
+            ->get();
+            return response()->json([
+
+                'response' => Response::HTTP_OK,
+                'success' => true,
+                'msg' => 'Fetch all',
+                'data' => RequestsResource::collection($respons)
+
+            ], Response::HTTP_OK);
+            
+        } catch (QueryException $e) {
+            return response()->json([
+
+                'response' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'success' => false,
+                'msg' => $e->getMessage(),
+                'data' => []
+
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function filter($goldar){
-        $respons = DB::table('requests')
-        ->join('rs', 'requests.rs_id', '=', 'rs.id')
-        ->where('requests_goldar', '=', $goldar)
-        ->get();
-        return response()->json(['data' => $respons]);
+        try {
+            $respons = DB::table('requests')
+            ->join('rs', 'requests.rs_id', '=', 'rs.id')
+            ->join('users', 'requests.user_id', '=', 'users.id')
+            ->where('requests_goldar', '=', $goldar)
+            ->get();
+            return response()->json([
+
+                'response' => Response::HTTP_OK,
+                'success' => true,
+                'msg' => 'Fetch all',
+                'data' => RequestsResource::collection($respons)
+
+            ], Response::HTTP_OK);
+            
+        } catch (QueryException $e) {
+            return response()->json([
+
+                'response' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'success' => false,
+                'msg' => $e->getMessage(),
+                'data' => []
+
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function detail($id){
         $respons = DB::table('requests')
         ->join('rs', 'requests.rs_id', '=', 'rs.id')
+        ->join('users', 'requests.user_id', '=', 'users.id')
         ->where('requests.id', '=', $id)
         ->get();
         return response()->json($respons);
---
+++ app/Http/Resources/RequestsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RequestsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return[
+            'id' => $this->id,
+            'Pasien' => $this->requests_pasien,
+            'JenisDonor' => $this->requests_jenis,
+            'Rs' => $this->rs_nama
+        ];
+    }
+}
