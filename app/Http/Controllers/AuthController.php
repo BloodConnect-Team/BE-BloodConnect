8dabdd12982386757bca913c1057f129fcf48360
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-03-25 00:30:41 +0700

    panambahan jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'goldar' => 'required'
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+
+                'response' => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'success' => false,
+                'message' => $validator->errors(),
+                'data' => []
+
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }else{
+            $user = new User;
+            $user->name  = $request->name;
+            $user->email  = $request->email;
+            $user->goldar  = $request->goldar;
+            $user->password  = Hash::make($request->password);
+            $user->remember_token  = Str::random(60);
+            $respons = $user->save();
+    
+            return response()->json([
+
+                'response' => Response::HTTP_OK,
+                'success' => true,
+                'message' => 'Requests created successfully.',
+                'data' => $respons
+
+            ], Response::HTTP_OK);
+        }
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json([
+                'response' => 401,
+                'success' => false,
+                'message' => 'Unauthorized',
+                'data' => [],
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'response' => Response::HTTP_OK,
+            'success' => true,
+            'message' => 'Successfully logged out',
+            'data' => [],
+        ], Response::HTTP_OK);    
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RequestsController;
 
 /*
@@ -14,6 +15,14 @@ use App\Http\Controllers\RequestsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+  Route::post('register', [AuthController::class, 'register']);
+  Route::post('login', [AuthController::class, 'login']);
+  Route::post('logout', [AuthController::class, 'logout']);
+  Route::post('refresh', [AuthController::class, 'refresh']);
+  Route::post('me', [AuthController::class, 'me']);
+});
 
 Route::get('/getReq', [RequestsController::class, 'index']);
 Route::get('/getReq/{goldar}', [RequestsController::class, 'filter']);
